7962cb308bf852f851b3ef53e019b4a0f821de1f
unknown <130582762+Y2riq@users.noreply.github.com>
2026-05-21 20:16:31 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -327,7 +327,7 @@ export default function App() {
         <h2 className="mb-4 text-4xl font-bold">Mari Terhubung</h2>
 
         <p className="mx-auto mb-8 max-w-md opacity-70">
-          Terbuka untuk peluang kerja di bidang Administrasi, Finance, Customer Service, dan
+          Terbuka untuk peluang kerja di bidang Administrasi, Customer Service, dan
           Operasional Perbankan.
         </p>
 
